f18e573bcca50ca848d0ca9620daee80d59837e7
FirestormPenguin <121073200+FirestormPenguin@users.noreply.github.com>
2023-02-06 17:37:39 +0100

    Add files via upload
    Calloc updated

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 15:08:46 by egiubell          #+#    #+#             */
-/*   Updated: 2023/01/30 15:01:48 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:28:43 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count == SIZE_MAX && size == SIZE_MAX)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
